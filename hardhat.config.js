a817f33707db19324e61e44e549c169b682d2c09
Prathik Reddy <tvkp333555@gmail.com>
2023-05-01 19:15:58 +0530

    backend work

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,32 +1,52 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config()
 require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter")
 
 module.exports = {
 
   solidity: "0.8.6",
-  // settings:{
-  //   optimizer:{
-  //     enabled:true,
-  //     runs:200
-  //   }
-  
+
+
+  gasReporter:{
+    enabled:true,
+    currency:"INR",
+    coinmarketcap:process.env.COINMARKETCAP,
+    token:"matic",
+    outputFile:"gasReports.txt",
+    noColors:true
+  },
+  settings:{
+    optimizer:{
+      enabled:true,
+      runs:200
+    }
+  },
+
+
   networks:{
     hardhat:{
       chainId:1337
     },
+
     mumbai:{
-      url:`https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API}`,
+      url:`https://few-quick-wave.matic-testnet.discover.quiknode.pro/${process.env.QUICKNODE_API_POLYGON}`,
       accounts:[process.env.MAIN_ACCOUNT],
-      chainId:80001,
+      chainId:80001
     },
- 
+    sepolia:{
+      url:`https://sepolia.infura.io/v3/${process.env.INFURA_API_SEPOLIA}`,
+      accounts:[process.env.MAIN_ACCOUNT],
+      chainId:11155111,
+    }
+
     //go to metamask and select mumbai testnet
     //go to account details from the three dots option
     //select export private key
-    //copy and paste pvt key .env Main_account 
+    //copy and paste pvt key .env Main_account
 
-  }
+  },
+
 }
     // goerli:{
     //   url: `https://goerli.infura.io/v3/${process.env.INFURA_API}`,
@@ -39,14 +59,3 @@ module.exports = {
     //   chainId:80001,
     // },
 
-
-  
-  // gasReporter:{
-  //   enabled:true,
-  //   currency:"INR",
-  //   coinmarketcap:process.env.COINMARKETCAP,
-  //   token:"matic",
-  //   outputFile:"gasReports.txt",
-  //   noColors:true
-  // }
-  
